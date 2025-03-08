3baea65bdd2025483682fee56d358f4e0c0a9fdb
Brabem <2198702716@qq.com>
2025-03-09 00:26:35 +0800

    feat: add stack code

--- dataStructureAndAlgorithms/stack.go
+++ dataStructureAndAlgorithms/stack.go
@@ -5,3 +5,35 @@ type StackInterface interface {
 	poll() int
 	getTop() int
 }
+
+type stack struct {
+	arr []int
+	len int
+	cap int
+}
+
+func (s *stack) push(i int) {
+	s.arr[s.len] = i
+	return
+}
+
+func (s *stack) poll() int {
+	val := s.arr[s.len]
+	for i := 0; i < s.len; i++ {
+		s.arr[i] = s.arr[i+1]
+	}
+
+	return val
+}
+
+func (s *stack) getTop() int {
+	return s.arr[s.len]
+}
+
+func NewStack(cap int) StackInterface {
+	return &stack{
+		arr: []int{},
+		len: 0,
+		cap: cap,
+	}
+}
