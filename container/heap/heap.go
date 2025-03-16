f0a6693d11e639b233b8a67cc5c3ca952868f554
Brabem <2198702716@qq.com>
2025-03-16 17:55:45 +0800

    feat: add release a top heap code

--- container/heap/heap.go
+++ container/heap/heap.go
@@ -43,3 +43,44 @@ func (kl *KthLargest) Add(val int) int {
 
 // 关于 Heap 包
 // 它为任何实现 heap.Interface{} 的类型提供堆操作。默认支持最小堆，但是可以通过自定义 Less 方法实现最大堆。
+
+// 滑动窗口最大值
+// 维护一个大顶堆进行解题
+var a []int
+
+type hp struct{ sort.IntSlice }
+
+func (h hp) Less(i, j int) bool {
+	return a[h.IntSlice[i]] > a[h.IntSlice[j]]
+}
+
+func (h *hp) Push(v interface{}) {
+	h.IntSlice = append(h.IntSlice, v.(int))
+}
+
+func (h *hp) Pop() interface{} {
+	a := h.IntSlice
+	v := a[len(a)-1]
+	h.IntSlice = a[:len(h.IntSlice)-1]
+	return v
+}
+
+func maxSlidingWindow(nums []int, k int) []int {
+	a = nums
+	q := &hp{make([]int, k)}
+	for i := 0; i < k; i++ {
+		q.IntSlice[i] = i
+	}
+	heap.Init(q)
+	n := len(nums)
+	ans := make([]int, 1, n-k+1)
+	ans[0] = nums[q.IntSlice[0]]
+	for i := k; i < n; i++ {
+		heap.Push(q, i)
+		for q.IntSlice[0] <= i-k {
+			heap.Pop(q)
+		}
+		ans = append(ans, nums[q.IntSlice[0]])
+	}
+	return ans
+}
