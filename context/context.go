91bafa95d72301fbbe45ad34aa032b4348e6eec9
Brabem <2198702716@qq.com>
2025-03-22 16:51:10 +0800

    feat: add context code

--- context/context.go
+++ context/context.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 )
 
 func main() {
-	//ctx := context.Background()
-	//c, cancel := context.WithTimeout(ctx, 10*time.Second)
-	//go func() {
-	//	time.Sleep(5 * time.Second)
-	//	cancel()
-	//}()
-	//
-	//fmt.Println(struct{}{} == <-c.Done())
+	ctx := context.Background()
+	c, cancel := context.WithTimeout(ctx, 10*time.Second)
+	go func() {
+		time.Sleep(5 * time.Second)
+		cancel()
+	}()
+
+	fmt.Println(struct{}{} == <-c.Done())
 
 	ch := make(chan struct{})
 	go func() {
