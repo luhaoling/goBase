7e48090bc211647b66592635067c4dc0a7b0967e
Brabem <2198702716@qq.com>
2025-04-08 00:16:22 +0800

    feat: add performaceOptimization tips

--- goServerDevelop/HighPerformancePracitices/SimgleMachePerformaceOptimization/first/first.go
+++ goServerDevelop/HighPerformancePracitices/SimgleMachePerformaceOptimization/first/first.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	slices := getRawSlices()
+	slices := getCapacitySlices()
 	getCapacitySet(slices)
 	// 设置响应头，这里设置Content-Type为text/plain，表示返回纯文本内容
 	w.Header().Set("Content-Type", "text/plain")
@@ -17,7 +17,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	// 注册路由，当客户端访问根路径"/"时，会调用Handler函数进行处理
 	http.HandleFunc("/", Handler)
-	err := http.ListenAndServe(":8884", nil)
+	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		panic(err)
 	}
@@ -27,6 +27,15 @@ func main() {
 func getRawSlices() []int {
 	n := 10000
 	slices := make([]int, 0)
+	for i := 0; i < n; i++ {
+		slices = append(slices, i)
+	}
+	return slices
+}
+
+func getCapacitySlices() []int {
+	n := 10000
+	slices := make([]int, 0, 10000)
 	for i := 0; i < n; i++ {
 		slices = append(slices, i)
 	}
@@ -64,8 +73,12 @@ func getCapacitySet(slices []int) map[int]struct{} {
 // ab -n 30000 -c 2 http://127.0.0.1:8888/
 
 // 内存火焰图
-//go tool pprof -http :8889 http://127.0.0.1:8888/debug/pprof/heap?second=30
+// go tool pprof -http :8889 http://127.0.0.1:8888/debug/pprof/heap
 
 // 优化思路：
 // 当使用 map 构造集合时，可以将 value 类型设置为空结构体，空结构体不占用内存空间。这样可以帮我们降低内存消耗
 // 当创建 map 和切片对象时，如果可以提前确定容器容量，就可以传入 make 函数中，从而避免往集合中添加数据时出发扩容迁移，达到降低内存和 CPU 资源消耗的目的
+
+// 优化 tips:
+// 利用火焰图查看内存占用的时候，记得思考一下，当前代码的想法究竟对不对？
+// 感觉代码思路没问题，再思考一下，当存储的数据量达到一定程度的时候，思考一下使用的数据类型的底层数据结构会不会对内存有较大的影响
