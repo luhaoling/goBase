09715335157b2496ba74b03ebdee6fc18b5acafa
Brabem <2198702716@qq.com>
2025-04-22 00:03:34 +0800

    feat: add release  optimisticLock code

--- lock/optimisticLock.go
+++ lock/optimisticLock.go
@@ -12,38 +12,37 @@ import (
 // 无锁读取：读取数据时不加锁，同时记录数据的当前版本号或状态
 // 条件更新：更新时检查版本号是否与读取时一致，一致则更新成功，然后版本号递增；不一致则失败（需重试或处理冲突）
 
-type OptimisticData struct{
-	Value interface{}
+type OptimisticData struct {
+	Value   interface{}
 	Version int64
 }
 
-func (d *OptimisticData)Get()(interface{},int64){
-	version:=atomic.LoadInt64(&d.Version)
-	return d.Value,version
+func (d *OptimisticData) Get() (interface{}, int64) {
+	version := atomic.LoadInt64(&d.Version)
+	return d.Value, version
 }
 
-func (d *OptimisticData)Update(expectedVersion int64,newValue interface{})bool{
-	if atomic.CompareAndSwapInt64(&d.Version,expectedVersion,expectedVersion+1){
-		d.Value=newValue
+func (d *OptimisticData) Update(expectedVersion int64, newValue interface{}) bool {
+	if atomic.CompareAndSwapInt64(&d.Version, expectedVersion, expectedVersion+1) {
+		d.Value = newValue
 		return true
 	}
 	return false
 }
 
-type Account struct{
+type Account struct {
 	balance OptimisticData
 }
 
-
-func (a *Account)WithDraw(amount float64)error{
+func (a *Account) WithDraw(amount float64) error {
 	for {
-		currentBalance,currentVersion:=a.balance.Get()
-		current:=currentBalance.(float64)
-		if current<amount{
+		currentBalance, currentVersion := a.balance.Get()
+		current := currentBalance.(float64)
+		if current < amount {
 			return fmt.Errorf("余额不足")
 		}
-		newBalance:=current-amount
-		if a.balance.Update(currentVersion,newBalance){
+		newBalance := current - amount
+		if a.balance.Update(currentVersion, newBalance) {
 			return nil
 		}
 
@@ -51,16 +50,21 @@ func (a *Account)WithDraw(amount float64)error{
 	}
 }
 
-func main(){
-	account:=&Account{
-		balance:OptimisticData{Value:100.0},
+func main() {
+	account := &Account{
+		balance: OptimisticData{Value: 100.0},
 	}
 
 	var wg sync.WaitGroup
-	for i:=0;i<10;i++{
+	for i := 0; i < 10; i++ {
 		wg.Add(1)
-		go func(){
-			defer wg.Done(
-		}
+		go func() {
+			defer wg.Done()
+			if err := account.WithDraw(10); err != nil {
+				fmt.Println(err)
+			}
+		}()
 	}
+	wg.Wait()
+	fmt.Println("最终余额:", account.balance.Value)
 }
