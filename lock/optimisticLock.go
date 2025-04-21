cb1919568c6e97f2ff6f2a27b4b1c5327f04fd47
Brabem <2198702716@qq.com>
2025-04-22 00:07:15 +0800

    feat: add explain of optimisticLock

--- lock/optimisticLock.go
+++ lock/optimisticLock.go
@@ -17,12 +17,15 @@ type OptimisticData struct {
 	Version int64
 }
 
+// 获取当前值以及版本号
 func (d *OptimisticData) Get() (interface{}, int64) {
 	version := atomic.LoadInt64(&d.Version)
 	return d.Value, version
 }
 
+// 更新值，传入版本号和新的值
 func (d *OptimisticData) Update(expectedVersion int64, newValue interface{}) bool {
+	// 对比版本号，如果版本号相同，则修改值，否则返回 false
 	if atomic.CompareAndSwapInt64(&d.Version, expectedVersion, expectedVersion+1) {
 		d.Value = newValue
 		return true
@@ -46,7 +49,7 @@ func (a *Account) WithDraw(amount float64) error {
 			return nil
 		}
 
-		// 版本冲突，重试
+		// 版本冲突，根据业务进行下一步操作
 	}
 }
 
